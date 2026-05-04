9abaf235a3f49167324de211c11c2a4f4724d195
Mike Robinson <michael.robinson@asos.com>
2026-05-04 11:34:57 +0100

    Adds git ignore file and Test Data

---
+++ StoryVision/TestData/TestData.swift
@@ -0,0 +1,55 @@
+//
+//  TestData.swift
+//  StoryVision
+//
+//  Created by Michael Robinson on 04/05/2026.
+//
+
+import Foundation
+
+enum TestData {
+    static let eagleAndFox = eagleAndFoxString
+    static let lionAndMouse = lionAndMouseString
+    static let bearAndTravellers = bearAndTravellersString
+}
+
+private let bearAndTravellersString = """
+Two Travellers were on the road together, when a Bear suddenly appeared on the scene. Before
+he observed them, one made for a tree at the side of the road, and climbed up into the branches
+and hid there. The other was not so nimble as his companion; and, as he could not escape, he
+threw himself on the ground and pretended to be dead. The Bear came up and sniffed all round
+him, but he kept perfectly still and held his breath: for they say that a bear will not touch a dead
+body. The Bear took him for a corpse, and went away. When the coast was clear, the Traveller in
+the tree came down, and asked the other what it was the Bear had whispered to him when he
+put his mouth to his ear. The other replied, “He told me never again to travel with a friend who
+deserts you at the first sign of danger.”
+"""
+
+private let lionAndMouseString = """
+A Lion asleep in his lair was waked up by a Mouse running over his face. Losing his temper he
+seized it with his paw and was about to kill it. The Mouse, terrified, piteously entreated him to
+spare its life. “Please let me go,” it cried, “and one day I will repay you for your kindness.” The
+idea of so insignificant a creature ever being able to do anything for him amused the Lion so
+much that he laughed aloud, and good-humouredly let it go. But the Mouse’s chance came, after
+all. One day the Lion got entangled in a net which had been spread for game by some hunters,
+and the Mouse heard and recognised his roars of anger and ran to the spot. Without more ado it
+set to work to gnaw the ropes with its teeth, and succeeded before long in setting the Lion free.
+“There!” said the Mouse, “you laughed at me when I promised I would repay you: but now you
+see, even a Mouse can help a Lion.”
+"""
+
+private let eagleAndFoxString = """
+An Eagle and a Fox became great friends and determined to live near one another: they thought
+that the more they saw of each other the better friends they would be. So the Eagle built a nest
+at the top of a high tree, while the Fox settled in a thicket at the foot of it and produced a litter of
+cubs. One day the Fox went out foraging for food, and the Eagle, who also wanted food for her
+young, flew down into the thicket, caught up the Fox’s cubs, and carried them up into the tree
+for a meal for herself and her family. When the Fox came back, and found out what had
+happened, she was not so much sorry for the loss of her cubs as furious because she couldn’t get
+at the Eagle and pay her out for her treachery. So she sat down not far off and cursed her. But it
+wasn’t long before she had her revenge. Some villagers happened to be sacrificing a goat on a
+neighbouring altar, and the Eagle flew down and carried off a piece of burning flesh to her nest.
+There was a strong wind blowing, and the nest caught fire, with the result that her fledglings fell
+half-roasted to the ground. Then the Fox ran to the spot and devoured them in full sight of the
+Eagle.
+"""
