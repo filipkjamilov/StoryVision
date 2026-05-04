551c758316bbd438b32fd4bbd67651009f95d71a
Filip Kjamilov <filipkjamilov@gmail.com>
2026-05-04 11:15:08 +0200

    Initial Commit

---
+++ StoryVision/StoryVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryVisionApp.swift
+//  StoryVision
+//
+//  Created by Filip Kjamilov on 04.05.26.
+//
+
+import SwiftUI
+
+@main
+struct StoryVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
